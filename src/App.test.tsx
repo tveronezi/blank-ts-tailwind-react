c014f0d963659829e43802c3a4849f4bc4d1e987
Thiago Veronezi <tveronezi@gmail.com>
2022-10-18 17:50:50 -0400

    init

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('placeholder', () => {
+  render(<App />);
+  expect(screen.getByText(/center/i)).toBeInTheDocument();
+});
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-800 grid place-content-center">
+      <span className="text-xl text-white">center</span>
+    </div>
+  );
+}
+
+export default App;
