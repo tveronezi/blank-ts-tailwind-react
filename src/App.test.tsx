bffa5a97befb4e02f9e2e92dfb3c692163632a70
Thiago Veronezi <tveronezi@gmail.com>
2022-12-20 06:33:08 -0500

    Fmt and lint (#1)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('placeholder', () => {
-  render(<App />);
-  expect(screen.getByText(/center/i)).toBeInTheDocument();
+test("placeholder", () => {
+    render(<App />);
+    expect(screen.getByText(/center/i)).toBeInTheDocument();
 });
